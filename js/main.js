ffe11282819e8c1ba68e76b0137d1cf9b1bf6f8c
M00Alaa <mohamedalaa.ng@gmail.com>
2024-07-04 06:07:56 +0300

    get total price

--- js/main.js
+++ js/main.js
@@ -9,12 +9,17 @@ let count = document.getElementById('count');
 let category = document.getElementById('category');
 let submit = document.getElementById('submit');
 
-console.log(title, price, taxes, ads, discount, total, count, category, submit);
-
-
-
 
 // get total price
+function getTotal() {
+    if (price.value != '') {
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+        total.innerHTML = result;
+    } else {
+        total.innerHTML = '';
+    }
+}
+
 // create product
 // save data to local storage
 // clear form data after submit
